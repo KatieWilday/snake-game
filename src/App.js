b1505f10c87463c2fadf88bf845d33affbd8ae41
Katie Wilday <wildayk@gmail.com>
2020-04-21 20:59:46 -0400

    firt commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Snake from './Snake';
+import Food from './Food';
+
+
+
+class App extends Component {
+
+  state = {
+    snakeDots: [
+      [0,0],
+      [2,0]
+    ]
+  }
+  render() {
+    return (
+      <div className="game-area">
+        <Snake snakeDots={this.state.snakeDots}/>
+      </div>
+    );
+  }
+}
+
+export default App;
