66becd954e4a9ac505b9124a21d56b017360c0ab
Katie Wilday <wildayk@gmail.com>
2020-04-21 21:05:09 -0400

    adding state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import Snake from './Snake';
 import Food from './Food';
 
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y =  Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [x,y]
+}
 
+const initialState = {
+  food: getRandomCoordinates(),
+  speed: 200,
+  direction: 'RIGHT',
+  snakeDots: [
+    [0,0],
+    [2,0]
+  ]
+}
 
 class App extends Component {
 
